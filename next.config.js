7d865c9d15a440f33553956dd8c82a046b7d5476
tdijkmans@gmail.com <tdijkmans@gmail.com>
2023-10-28 20:19:07 +0200

    adding export configuration

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    output: 'export',
+    reactStrictMode: true,
+    distDir: '_static',
+    images: {
+        unoptimized: true,
+    },
+}
 
 module.exports = nextConfig
