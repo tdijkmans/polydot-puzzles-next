9fdc14dc00af20d2114ab97b8991d4a1af40d846
Thomas Dijkmans <tdijkmans@gmail.com>
2024-09-30 10:30:59 +0200

    fix: Update text encoding for special characters in book and instruction tabs; refactor Earth component texture loading

--- app/components/exampleTab/ExampleTab.tsx
+++ app/components/exampleTab/ExampleTab.tsx
@@ -9,7 +9,7 @@ const ExampleTab = () => {
             <h2 className={styles.title}>Polydot Drawing of The Golem</h2>
             <GolemLine />
             <div className={styles.polyDotdescription}>
-                With each step, it protects its creator's home, its massive fists ready to defend against any threat
+                With each step, it protects its creator&apos;s home, its massive fists ready to defend against any threat
             </div>
         </div>)
 };
