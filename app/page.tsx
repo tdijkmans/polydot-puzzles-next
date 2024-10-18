8247d53408da87b294b9c463ee5799dc9b5e1bb3
Thomas Dijkmans <tdijkmans@gmail.com>
2024-10-18 14:29:43 +0200

    feat: Update headings to h2 for consistency; add SEO metadata for BookTab; enhance BookTab description and layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import styles from "./page.module.css";
 import IntroTab from "./tabs/AboutPolydots/IntroTab";
 import BookTab from "./tabs/BookTab/bookTab";
@@ -8,6 +9,13 @@ import Signup from "./tabs/SignupTab/Signup";
 export default function Page() {
 	return (
 		<>
+			<Head>
+				<title>Polydot Puzzles - Low Poly Connect-the-Dots for Adults</title>
+				<meta
+					name="description"
+					content="Low Poly Connect-the-Dots for Adults"
+				/>
+			</Head>
 			<div className={styles.tabs}>
 				<div className={styles.tab}>
 					<BookTab />
