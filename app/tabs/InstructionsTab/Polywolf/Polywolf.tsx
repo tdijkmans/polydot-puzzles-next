aefd76efa8b21f82cb90e87f6b70ed445f8a9127
Thomas Dijkmans <tdijkmans@gmail.com>
2024-10-29 11:00:19 +0100

    feat: Refactor class name assignment in Polywolf component for improved readability and maintainability

--- app/tabs/InstructionsTab/Polywolf/Polywolf.tsx
+++ app/tabs/InstructionsTab/Polywolf/Polywolf.tsx
@@ -5,6 +5,33 @@ import Letters from "../Letters/Letters";
 import styles from "./Polywolf.module.css";
 import { steps } from "./polyWolfData";
 
+const getClassName = (
+	offsetStep: number,
+	index: number,
+	type: "line" | "circle" | "text",
+) => {
+	let suffix: "Past" | "Previous" | "Current" | "Next" | "Future";
+
+	switch (true) {
+		case index < offsetStep:
+			suffix = "Past";
+			break;
+		case index === offsetStep - 1:
+			suffix = "Previous";
+			break;
+		case index === offsetStep:
+			suffix = "Current";
+			break;
+		case index === offsetStep + 1:
+			suffix = "Next";
+			break;
+		default:
+			suffix = "Future";
+	}
+
+	return `${styles[type]} ${styles[`${type}${suffix}`]}`;
+};
+
 const Polywolf = () => {
 	const [curStep, setCurStep] = useState(-1);
 
@@ -54,31 +81,9 @@ const Polywolf = () => {
 							const { cx, cy, label } = dot;
 
 							const offsetStep = curStep + 1;
-							let circleClassName = styles.circle;
-							if (offsetStep === index) {
-								circleClassName += ` ${styles.circleCurrent}`;
-							} else if (offsetStep === index + 1) {
-								circleClassName += ` ${styles.circleNext}`;
-							} else if (offsetStep === index - 1) {
-								circleClassName += ` ${styles.circlePrevious}`;
-							} else if (offsetStep > index) {
-								circleClassName += ` ${styles.circlePast}`;
-							} else {
-								circleClassName += ` ${styles.circleFuture}`;
-							}
-
-							let textClassName = styles.text;
-							if (offsetStep === index) {
-								textClassName += ` ${styles.textCurrent}`;
-							} else if (offsetStep === index + 1) {
-								textClassName += ` ${styles.textNext}`;
-							} else if (offsetStep === index - 1) {
-								textClassName += ` ${styles.textPrevious}`;
-							} else if (offsetStep > index) {
-								textClassName += ` ${styles.textPast}`;
-							} else {
-								textClassName += ` ${styles.textFuture}`;
-							}
+							const isNextStep = offsetStep + 1 === index;
+							const circleClassName = getClassName(offsetStep, index, "circle");
+							const textClassName = getClassName(offsetStep, index, "text");
 
 							return (
 								<Fragment key={`circle_${label}_${step}`}>
@@ -90,7 +95,7 @@ const Polywolf = () => {
 										className={circleClassName}
 									/>
 
-									{offsetStep + 1 === index && (
+									{isNextStep && (
 										<circle
 											cx={cx}
 											cy={cy}
@@ -116,33 +121,17 @@ const Polywolf = () => {
 						d="m294.71 630.917-71.507-33.615M223.203 597.302l-109.888 6.639M113.315 603.94l-62.742-29.277M50.573 574.663 8.128 513.11M8.128 513.11 1.05 467.829M1.05 467.829l162.668-93.32M163.718 374.51l14.78-13.903M178.498 360.607l-29.213-53.16M149.285 307.447l14.433 67.063M163.718 374.51l86.725 7.76M250.443 382.27l91.55 11.339M341.994 393.609l19.008-42.023M361.002 351.586l-65.035-21.51M295.967 330.076l-45.524 52.194M250.443 382.27l18.008-84.712M268.45 297.558l56.537-20.511M324.987 277.047l45.523-66.537M370.51 210.51l1.267-45.49M371.777 165.02l-106.123 30.424M265.654 195.444l-66.681 59.25M198.973 254.694l-49.688 52.753M149.285 307.447l29.119-19.898M178.404 287.549 227.612 311M227.612 311l14.713-76.933M242.325 234.067l23.326-38.621M198.973 254.694l-20.696-61.727M178.277 192.967l15.916-175.102M194.193 17.865 371.777 165.02M371.777 165.02 424.134 3.714M424.134 3.713l186.068 295.024M610.202 298.737l-198.615 387.12M411.587 685.857l-56.03-26.214"
 					/>
 					<g id="lines_group">
-						{steps.map(({ line }, index) => {
-							let className = styles.line;
-
-							if (index < curStep) {
-								className += ` ${styles.linePast}`;
-							} else if (index === curStep - 1) {
-								className += ` ${styles.linePrevious}`;
-							} else if (index === curStep) {
-								className += ` ${styles.lineCurrent}`;
-							} else if (index === curStep + 1) {
-								className += ` ${styles.lineNext}`;
-							} else {
-								className += ` ${styles.lineFuture}`;
-							}
-
-							return (
-								<line
-									key={`line_${line.x1}_${line.y1}_${index}`}
-									x1={line.x1}
-									y1={line.y1}
-									x2={line.x2}
-									y2={line.y2}
-									id={line.lineId}
-									className={className}
-								/>
-							);
-						})}
+						{steps.map(({ line }, index) => (
+							<line
+								key={`line_${line.x1}_${line.y1}_${index}`}
+								x1={line.x1}
+								y1={line.y1}
+								x2={line.x2}
+								y2={line.y2}
+								id={line.lineId}
+								className={getClassName(curStep, index, "line")}
+							/>
+						))}
 					</g>
 
 					<path
